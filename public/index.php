d9057c7f0e44a01eba05671e897c826ffb32e9e0
SAURABH SAMIR - MFCWL <23269636@mahindra.com>
2024-12-30 11:53:40 +0530

    Fixed configuration issues

--- public/index.php
+++ public/index.php
@@ -1,17 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
+// Set the correct document root to point to the public directory of Laravel.
+$_SERVER['DOCUMENT_ROOT'] = __DIR__ . '/public';
 
-define('LARAVEL_START', microtime(true));
-
-// Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
-    require $maintenance;
-}
-
-// Register the Composer autoloader...
-require __DIR__.'/../vendor/autoload.php';
-
-// Bootstrap Laravel and handle the request...
-(require_once __DIR__.'/../bootstrap/app.php')
-    ->handleRequest(Request::capture());
+// Include the Laravel app's entry point (public/index.php)
+require_once __DIR__ . '/public/index.php';
